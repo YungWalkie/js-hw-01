235a608a239b4dd541ccb14d9cf54352d0b14534
YungWalkie <dmitriyzdrokwork@gmail.com>
2021-05-17 01:40:20 +0200

    Finished second homework

---
+++ js/index01.js
@@ -0,0 +1,9 @@
+const total = 100;
+let ordered = 50;
+ordered>total ? console.log('"На складе недостаточно твоаров!"') : console.log('"Заказ оформлен, с вами свяжется менеджер"')
+ordered = 20;
+ordered>total ? console.log('"На складе недостаточно твоаров!"') : console.log('"Заказ оформлен, с вами свяжется менеджер"')
+ordered = 80;
+ordered>total ? console.log('"На складе недостаточно твоаров!"') : console.log('"Заказ оформлен, с вами свяжется менеджер"')
+ordered = 130;
+ordered>total ? console.log('"На складе недостаточно твоаров!"') : console.log('"Заказ оформлен, с вами свяжется менеджер"')
