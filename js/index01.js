125799248d3fa900ee7013f313d925def4f9afdc
YungWalkie <dmitriyzdrokwork@gmail.com>
2021-05-17 09:56:52 +0200

    Semplified code

--- js/index01.js
+++ js/index01.js
@@ -1,9 +1,11 @@
 const total = 100;
 let ordered = 50;
-ordered>total ? console.log('"На складе недостаточно твоаров!"') : console.log('"Заказ оформлен, с вами свяжется менеджер"')
+let messageNotEnough = '"На складе недостаточно твоаров!"';
+let messageEnough = '"Заказ оформлен, с вами свяжется менеджер"';
+ordered>total ? console.log(messageNotEnough) : console.log(messageEnough)
 ordered = 20;
-ordered>total ? console.log('"На складе недостаточно твоаров!"') : console.log('"Заказ оформлен, с вами свяжется менеджер"')
+ordered>total ? console.log(messageNotEnough) : console.log(messageEnough)
 ordered = 80;
-ordered>total ? console.log('"На складе недостаточно твоаров!"') : console.log('"Заказ оформлен, с вами свяжется менеджер"')
+ordered>total ? console.log(messageNotEnough) : console.log(messageEnough)
 ordered = 130;
-ordered>total ? console.log('"На складе недостаточно твоаров!"') : console.log('"Заказ оформлен, с вами свяжется менеджер"')
+ordered>total ? console.log(messageNotEnough) : console.log(messageEnough)
